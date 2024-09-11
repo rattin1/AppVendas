39cc57b27a3819c0e27fa84dbd2715e82f40ea98
rattini <rattinicursods@gmail.com>
2024-09-11 17:01:56 -0300

    Adição de Classes

---
+++ AppVendas/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace AppVendas.Models
+{
+    public class Produto
+    {
+        public Guid ProdutoId { get; set; }
+        public string ProdutoNome { get; set; }
+        public string Valor { get; set; }
+        public string QtadeEstoque { get; set; }
+        public bool CadastroAtivo { get; set; }
+
+        /* Chave Estrangeira */
+        public Guid CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+    }
+}
---
+++ AppVendas/Models/Venda.cs
@@ -0,0 +1,15 @@
+﻿namespace AppVendas.Models
+{
+    public class Venda
+    {
+        public Guid VendaId { get; set; }
+        public int NotaFiscal { get; set; }
+        public DateTime DataVenda { get; set; }
+        public double? TotalVenda { get; set; }
+
+        /* Referencia para a Model Cliente */
+
+        public Guid ClienteId { get; set; }
+        public Cliente? Cliente { get; set; }
+    }
+}
